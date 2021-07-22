fb66382111e581f0d75cdfa5e6c2ea7bbf81a110
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-07-22 12:19:22 +0300

    Add files via upload

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Post
 from .models import Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -14,8 +15,8 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-' 
+    empty_value_display = '-пусто-'
 
-admin.site.register(Post, PostAdmin) 
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
-
