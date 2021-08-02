93315ec1b2ad040b73767923afae5450423e40cb
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-08-02 12:37:34 +0300

    a

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -20,3 +20,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
+
---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+
+    context = {
+        'posts': posts
+    }
+    return render(request, template, context)
